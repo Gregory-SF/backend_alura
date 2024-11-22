88f084dcbdfdab30b701db47bc80f5e7ac92e903
Gregory Soares <gregory16704@gmail.com>
2024-11-22 11:53:58 -0300

    Quarta aula, implementando armazenamento e upload de imagens

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-
-routes(app);
+routes(app)
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,25 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ dest: "./uploads" , storage});
 
 const routes = (app) => {
-    app.use(express.json()); // Habilita o middleware para analisar o corpo das requisições JSON
+    app.use(express.json()); // Permite que o servidor interprete requisições com corpo no formato JSON
     app.get("/posts", listarPosts);
+    // Rota parar criar um post
+    app.post("/posts", postarNovoPost);
+    // Rota para postar imagem
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 };
 
 export default routes;
